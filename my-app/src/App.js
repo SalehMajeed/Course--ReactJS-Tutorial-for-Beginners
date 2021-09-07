899afaf4d3ce151728f784cd2de3aab174e6fe50
salehmajeed <saleh.majeed8@gmail.com>
2021-09-07 14:55:00 +0530

    my first react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,18 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
-import { Greet } from './components/greet';
-import Classcomponent from './components/welcome';
-import Jsx from './components/hello';
-import Message from './components/message';
 
-class App extends Component {
-	render() {
-		return (
-			<div className='App'>
-				<Greet name='Bruce'>For Bruce</Greet>
-				<Greet name='Ben'>Kill Bruce</Greet>
-				<Classcomponent name='Bruce'>For Bruce</Classcomponent>
-				<Classcomponent name='Ben'>Kill Bruce</Classcomponent>
-				<Jsx />
-				<Message message='User'></Message>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>Hello World</p>
+				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
