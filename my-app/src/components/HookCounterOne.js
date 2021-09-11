64c587394a98017b0ff997e22db00c905a7e1c39
salehmajeed <saleh.majeed8@gmail.com>
2021-09-11 16:28:04 +0530

    useEffect with second argument for condition

--- my-app/src/components/HookCounterOne.js
+++ my-app/src/components/HookCounterOne.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 function HookCounterOne() {
 	const [count, setCount] = useState(0);
-	useEffect(() => (document.title = `You clicked ${count} times`));
+	const [name, setName] = useState('');
+
+	useEffect(() => {
+		console.log('use effect -- updating document title');
+		document.title = `You clicked ${count} times`;
+	}, [count]);
 	return (
 		<div>
+			<input type='text' value={name} onChange={e => setName(e.target.value)} />
 			<button onClick={() => setCount(count + 1)}>Click {count} times</button>
 		</div>
 	);
