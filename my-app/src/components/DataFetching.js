bd4e9c538295d5f0668779c897f5b13ef1ec1bd8
salehmajeed <saleh.majeed8@gmail.com>
2021-09-12 13:03:55 +0530

    fetching with button

--- my-app/src/components/DataFetching.js
+++ my-app/src/components/DataFetching.js
@@ -4,19 +4,27 @@ import axios from 'axios';
 function DataFetching() {
 	const [posts, setPosts] = useState({});
 	const [id, setId] = useState(1);
+	const [idFromButton, setIdFromButton] = useState(1);
+
+	const handle_click = () => {
+		setIdFromButton(id);
+	};
 
 	useEffect(() => {
 		axios
-			.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+			.get(`https://jsonplaceholder.typicode.com/posts/${idFromButton}`)
 			.then(res => {
 				console.log(res);
 				setPosts(res.data);
 			})
 			.catch(err => [console.log(err)]);
-	}, [id]);
+	}, [idFromButton]);
 	return (
 		<div>
 			<input type='text' value={id} onChange={e => setId(e.target.value)} />
+			<button type='button' onClick={handle_click}>
+				Fetch Post
+			</button>
 			{/* <ul>
 				{posts.map(post => (
 					<li key={post.id}>{post.title}</li>
