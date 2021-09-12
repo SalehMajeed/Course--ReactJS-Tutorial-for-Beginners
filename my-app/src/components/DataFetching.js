e6a397ab85f17256fbc7c74e4df74ed53084a919
salehmajeed <saleh.majeed8@gmail.com>
2021-09-12 13:00:11 +0530

    fetching individual post using useEffect

--- my-app/src/components/DataFetching.js
+++ my-app/src/components/DataFetching.js
@@ -2,23 +2,27 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function DataFetching() {
-	const [posts, setPosts] = useState([]);
+	const [posts, setPosts] = useState({});
+	const [id, setId] = useState(1);
+
 	useEffect(() => {
 		axios
-			.get('https://jsonplaceholder.typicode.com/posts')
+			.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
 			.then(res => {
 				console.log(res);
 				setPosts(res.data);
 			})
 			.catch(err => [console.log(err)]);
-	}, []);
+	}, [id]);
 	return (
 		<div>
-			<ul>
+			<input type='text' value={id} onChange={e => setId(e.target.value)} />
+			{/* <ul>
 				{posts.map(post => (
 					<li key={post.id}>{post.title}</li>
 				))}
-			</ul>
+			</ul> */}
+			<div>{posts.title}</div>
 		</div>
 	);
 }
