5cd5de798e5dbafb2728a2f74d1507dff3bbef73
salehmajeed <saleh.majeed8@gmail.com>
2021-09-13 17:23:03 +0530

    useEffect with useReducer

--- my-app/src/components/ReducerDataFetching.js
+++ my-app/src/components/ReducerDataFetching.js
@@ -1,10 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
+
+const intial_state = {
+	loading: true,
+	error: '',
+	post: {},
+};
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'FETCH_SUCCESS':
+			return {
+				loading: false,
+				post: action.payload,
+				error: '',
+			};
+		case 'FETCH_ERROR':
+			return {
+				loading: false,
+				post: {},
+				error: 'Something went wrong',
+			};
+		default:
+			return state;
+	}
+};
 
 function ReducerDataFetching() {
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState('');
 	const [post, setPost] = useState({});
+
+	const [state, dispatch] = useReducer(reducer, intial_state);
 
 	useEffect(() => {
 		axios
@@ -22,10 +49,26 @@ function ReducerDataFetching() {
 			});
 	}, []);
 
+	useEffect(() => {
+		axios
+			.get('https://jsonplaceholder.typicode.com/posts/1')
+			.then(res => {
+				dispatch({
+					type: 'FETCH_SUCCESS',
+					payload: res.data,
+				});
+			})
+			.catch(err => {
+				dispatch({
+					type: 'FETCH_ERROR',
+				});
+			});
+	}, []);
+
 	return (
 		<div>
-			{loading ? 'Loading' : post.title}
-			{error ? error : null}
+			{state.loading ? 'Loading' : state.post.title}
+			{state.error ? state.error : null}
 		</div>
 	);
 }
