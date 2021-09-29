796d3f6ae43f70d246f99ba5ef55eb2c727b0a48
salehmajeed <saleh.majeed8@gmail.com>
2021-09-29 15:10:38 +0530

    useMemo and useCallack in rendering

--- my-app/src/components/IncorrectOptimization/Parent4.js
+++ my-app/src/components/IncorrectOptimization/Parent4.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import Child5 from './Child5';
 
 const ParentFour = ({ newCount, children }) => {
@@ -9,14 +9,19 @@ const ParentFour = ({ newCount, children }) => {
 		lname: 'Wayne',
 	};
 
+	const memoiz_person = useMemo(() => person, []);
+
 	const handle_click = () => {};
+	const memoiz_click = useCallback(handle_click, []);
 
 	console.log('ParentFour Render');
 	return (
 		<div>
 			<button onClick={() => setCount(c => c + 1)}>Count = {count}</button>
 			<button onClick={() => setName('Wayne')}>Change Name</button>
-			<Child5 name={name} person={person} handle_click={handle_click} />
+			{/* <Child5 name={name} person={memoiz_person}  /> */}
+			<Child5 name={name} person={memoiz_person} handle_click={memoiz_click} />
+			{/* <Child5 name={name} person={person} handle_click={handle_click} /> */}
 		</div>
 	);
 };
